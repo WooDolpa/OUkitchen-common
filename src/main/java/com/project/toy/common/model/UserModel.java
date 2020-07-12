b0923d4cb1b20a3aff0ae2794d6e671152e760c8
jwlee <dannis1124@naver.com>
2020-07-12 10:49:47 +0900

    사용자 모델 추가

---
+++ src/main/java/com/project/toy/common/model/UserModel.java
@@ -0,0 +1,21 @@
+package com.project.toy.common.model;
+
+import com.project.toy.common.enums.DataStatus;
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jwlee
+ * Date: 2020/07/12
+ */
+@Data
+public class UserModel {
+
+    private int userNo;                     // 사용자 번호
+    private String userId;                  // 아이디
+    private String userPassword;            // 비밀번호
+    private DataStatus dataStatus;          // 사용자 상태
+    private String regDatetime;             // 등록일
+    private String updDatetime;             // 수정일
+
+}
